e1d5e34c06c7a5a4f4b39e34932510332c12c732
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2024-05-17 13:41:35 -0400

    Update autonosnode.sh

--- autonosnode.sh
+++ autonosnode.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+#Automatically start Nosana Node on tty1 for Ubuntu Server
 
-# chmod +x autonosnode.sh
+#The objective of this script is to automatically launch Nosana Node on Ubuntu Server 22.04.
+#It will resmble operation of the node as if the user manually logged-in and ran the Nosana node start script in the cli.
+#This will be 100% familiar to a novice owner/operator who may already be doing the manual tasks.
+
+#Before running this script, the user will need to type the command: chmod +x autonosnode.sh
+
 #./autonosnode.sh
 # The objective of this script is to automatically launch Nosana Node on Ubuntu Server 22.04
 # It will resmble operation of the node as if the user manually logs-in and runs the Nosana start script.
