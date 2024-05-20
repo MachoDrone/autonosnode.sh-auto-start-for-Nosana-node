ee3545ececb74f3e3f615ebd91a517759bf6fe3c
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2024-05-20 07:45:09 -0400

    Update and rename autonosnode1.sh to autonosnode2.sh

--- autonosnode2.sh
+++ autonosnode2.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-# Automatically start Nosana Node on tty1 for Ubuntu Server
-# For Ubuntu Desktop, the GUI is likely already on tty1, so this project has a fork named autonosnode2.sh which auto starts Nosana node on tty2.
-# Starting on tty2 may be convenient for Ubuntu Desktop, but it is highly recommended to run from a terminal in the GUI for ease of screencaptures / support.
+# Automatically start Nosana Node on tty2 for Ubuntu Server and for Ubuntu Desktop where the GUI is likely to already be on tty1.
 
-# The objective of this script is to automatically launch Nosana Node on Ubuntu Server 22.04.
-# It will resmble operation of the node as if the user manually logged-in and ran the Nosana node start script in the cli.
-# This will be 100% familiar to a novice owner/operator who may already be doing the manual tasks.
-# Before running this script, the user will need to type the command: chmod +x autonosnode1.sh
+# Starting Nosana node on tty2 may be convenient for Ubuntu Desktop, but it is highly recommended to run Nosana node
+# from a terminal in the GUI for ease of screencaptures/support.
+
+# The objective of this script is to automatically launch Nosana Node on Ubuntu 22.04.
+# It will resmble operation of the node as if the user manually logged-in to tty2 and ran the Nosana node start script in the cli.
+# This will be mosty familiar to a novice owner/operator who may already be doing the manual tasks with addition to ctrl-alt-F2 to view Nosana node.
+# Before running this script, the user will need to type the command: chmod +x autonosnode2.sh
 
 # To take full advantage of this script, users will need to edit the BIOS of their PC to automatically restart the PC after power is reapplied.
 # e.g. after an outage with their mains utility/power company.
 
-#./autonosnode1.sh
+#./autonosnode2.sh
 # The objective of this script is to automatically launch Nosana Node on Ubuntu Server 22.04
 # It will resmble operation of the node as if the user manually logs-in and runs the Nosana start script.
 # This will be 100% familiar to a novice owner/operator who may already be doing the manual tasks.
@@ -30,14 +31,14 @@ draw_box() {
     echo "┃  by MachoDrone                                                v0.240517  ┃"
     echo "┃                                                                          ┃"
     echo "┃                                                                          ┃"
-    echo "┃      This script will configure autologin for Nosana node on tty1.       ┃"
+    echo "┃      This script will configure autologin for Nosana node on tty2.       ┃"
     echo "┃                                                                          ┃"
     echo "┃                                                                          ┃"
     echo "┃ **THIS IS FOR Ubuntu Server (non-GUI), NOT FOR WSL2 nor Ubuntu Desktop** ┃"
     echo "┃                                                                          ┃"
     echo "┃      It will perform the following actions:                              ┃"
     echo "┃          1. Append the Nosana node start script to .profile              ┃"
-    echo "┃          2. Configure autologin for tty1                                 ┃"
+    echo "┃          2. Configure autologin for tty2                                 ┃"
     echo "┃          3. Edit /etc/systemd/logind.conf to enable autologin            ┃"
     echo "┃                                                                          ┃"
     echo "┃      If the script has already been run, it will offer to uninstall.     ┃"
@@ -53,16 +54,16 @@ echo .
 # Function to append start script to .profile
 append_start_script() {
     # Append the start script for the Nosana node to .profile
-    printf '\n # Launch Nosana node start script\n if [[ -z "$DISPLAY" ]] && [[ $(tty) = /dev/tty1 ]]; then\n     bash <(wget -qO- https://nosana.io/testgrid.sh)\n fi\n' >> .profile
+    printf '\n # Launch Nosana node start script\n if [[ -z "$DISPLAY" ]] && [[ $(tty) = /dev/tty2 ]]; then\n     bash <(wget -qO- https://nosana.io/testgrid.sh)\n fi\n' >> .profile
 }
 
-# Function to configure autologin for tty1
+# Function to configure autologin for tty2
 configure_autologin() {
     # Create the directory for systemd service override
-    mkdir -p /etc/systemd/system/getty@tty1.service.d/
+    mkdir -p /etc/systemd/system/getty@tty2.service.d/
 
     # Set up autologin in override.conf
-    printf "[Service]\nExecStart=\nExecStart=-/sbin/agetty --noissue --autologin $SUDO_USER %%I \$TERM\nType=idle\n" | sudo tee /etc/systemd/system/getty@tty1.service.d/override.conf >/dev/null
+    printf "[Service]\nExecStart=\nExecStart=-/sbin/agetty --noissue --autologin $SUDO_USER %%I \$TERM\nType=idle\n" | sudo tee /etc/systemd/system/getty@tty2.service.d/override.conf >/dev/null
 }
 
 # Function to edit /etc/systemd/logind.conf to enable autologin
@@ -77,7 +78,7 @@ uninstall_script() {
     # Remove the appended lines from .profile
     sed -i '/# Launch Nosana node start script/,/# End of Nosana node start script/d' .profile
     # Remove the autologin configuration
-    sudo rm -rf /etc/systemd/system/getty@tty1.service.d/
+    sudo rm -rf /etc/systemd/system/getty@tty2.service.d/
     # Revert changes in logind.conf
     sudo sed -i 's/^NAutoVTs=/#NAutoVTs=/' /etc/systemd/logind.conf
     echo "Script uninstalled successfully."
@@ -125,7 +126,7 @@ echo .
 echo "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━.━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"
 echo "┃                                                            ┃"
 echo "┃    Store this script. It is the Installer & Uninstaller    ┃"
-echo "┃  Both simply use the same command:  sudo ./autonosnode1.sh ┃"
+echo "┃  Both simply use the same command:  sudo ./autonosnode2.sh ┃"
 echo "┃                                                            ┃"
 echo "┃ *In your PC's BIOS, setup auto-boot when power is applied* ┃"
 echo "┃                                                            ┃"
